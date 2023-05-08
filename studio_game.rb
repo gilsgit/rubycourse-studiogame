3cdf15d2a58e1f92897de2378f6b53cd89029373
Hello Good <codeo@lampstand.ca>
2023-05-08 09:35:04 -0400

    initial repo

---
+++ studio_game.rb
@@ -0,0 +1,18 @@
+name1 = "larry"
+name2 = "curly"
+name3 = "moe"
+name4 = "shemp"
+health1 = 30
+health2 = 125
+health3 = 100
+health4 = 90
+
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
+puts "#{name3.capitalize} has a health of #{health3}".center(50, '*')
+puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
+
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+
+puts "The game started on #{formatted_time}"
